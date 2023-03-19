4b35db39497f9d745585f78c58000d6f6d1dd2eb
Zalezny <zaleznypl@gmail.com>
2023-03-19 15:00:40 +0100

    added line

--- lib/pages/selected_order_page/selected_order_page_widgets/selected_order_cart_item.dart
+++ lib/pages/selected_order_page/selected_order_page_widgets/selected_order_cart_item.dart
@@ -7,48 +7,56 @@ class SelectedOrderCartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return IntrinsicHeight(
-      child: Row(
-        children: [
-          Image.network(
-            'https:${itemCart.photo}',
-            height: 150,
-            width: 100,
-          ),
-          Flexible(
-            flex: 1,
-            fit: FlexFit.tight,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  itemCart.title!,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
+    return Column(
+      children: [
+        IntrinsicHeight(
+          child: Row(
+            children: [
+              Image.network(
+                'https:${itemCart.photo}',
+                height: 150,
+                width: 100,
+              ),
+              Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      itemCart.title!,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: itemCart.color!.isEmpty
+                          ? Text(
+                              "rozmiar: ${itemCart.size}",
+                              textAlign: TextAlign.center,
+                            )
+                          : Text(
+                              "kolor: ${itemCart.color}",
+                              textAlign: TextAlign.center,
+                            ),
+                    ),
+                    Text("ilość: ${itemCart.quantity}"),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: itemCart.color!.isEmpty
-                      ? Text(
-                          "rozmiar: ${itemCart.size}",
-                          textAlign: TextAlign.center,
-                        )
-                      : Text(
-                          "kolor: ${itemCart.color}",
-                          textAlign: TextAlign.center,
-                        ),
-                ),
-                Text("ilość: ${itemCart.quantity}"),
-              ],
-            ),
+              ),
+              Text("${itemCart.price} zł",
+                  style: Theme.of(context).textTheme.titleLarge),
+            ],
           ),
-          Text("${itemCart.price} zł",
-              style: Theme.of(context).textTheme.titleLarge),
-        ],
-      ),
+        ),
+        const Divider(
+          thickness: 1,
+          color: Colors.black,
+        )
+      ],
     );
   }
 }
